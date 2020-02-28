b53d1e59c66407ee28e387b4d0b3f91c7ffe6b45
deep08 <deep.pate@edu.ece.fr>
2020-02-28 18:50:36 +0100

    Une basic navigation mis en place avec des fonctions et librairies ext

--- App.js
+++ App.js
@@ -1,23 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Register from './Components/Register'
 import Login from './Components/Login'
 import { render } from 'react-dom';
 import Model from './Components/Model/Model'
-import Navigation from './Navigation/Navigation'
+//import Navigation from './Navigation/Navigation'
 
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+/*
 export default class App extends React.Component {
   render(){
     return (
-
-       
         <Navigation/> 
-      
-   
-
       );
    }
 }
+*/
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+      
+
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -9,7 +9,7 @@ import { createStackNavigator, createAppContainer } from 'react-navigation-stack
 
 const SearchStackNavigator = createStackNavigator({
   Login: { // Ici j'ai appelé la vue "Search" mais on peut mettre ce que l'on veut. C'est le nom qu'on utilisera pour appeler cette vue
-    login: Login,
+    screen: Login,
     navigationOptions: {
       title: 'Rechercher'
     }
