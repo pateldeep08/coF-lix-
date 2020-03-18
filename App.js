82f85de9bc42451ff0edc8029ffabe4ddce644ca
kevinfrydmanesqd <54984269+kevinfrydmanesqd@users.noreply.github.com>
2020-03-18 09:37:29 +0100

    Update App.js
    commentaire codereview

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Register from './Components/Register'
 import Login from './Components/Login'
-import { render } from 'react-dom';
+//import { render } from 'react-dom'; cette ligne là est inutile, render existe déja dans la classe parent (Component).
 
+/*
+ * D'un point de vue écriture de code, je trouve ça plus classe d'écrire class App extends Component {
+ * Ne pas oublier du coup d'importer Component et faire à la toute fin export default App;
+ */
 export default class App extends React.Component {
   render(){
     return (
